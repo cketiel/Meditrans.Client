fc61b6e6df6965410e86f19308dab827f60a3b37
cketiel <cketielbr@gmail.com>
2025-04-28 16:21:18 -0600

    Show change language menu, translations in the main window

---
+++ Models/LanguageOption.cs
@@ -0,0 +1,26 @@
+﻿
+using Meditrans.Client.ViewModels;
+
+namespace Meditrans.Client.Models
+{
+    public class LanguageOption : BaseViewModel
+    {
+        public string LanguageCode { get; set; }
+        public string DisplayName { get; set; }
+        public string FlagEmoji { get; set; }
+
+        private bool _isSelected;
+        public bool IsSelected
+        {
+            get => _isSelected;
+            set
+            {
+                _isSelected = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(CheckVisibility));
+            }
+        }
+
+        public System.Windows.Visibility CheckVisibility => IsSelected ? System.Windows.Visibility.Visible : System.Windows.Visibility.Collapsed;
+    }
+}
--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,9 @@
-﻿using System.Windows;
+﻿using System.Collections.ObjectModel;
+using System.Windows;
 using System.Windows.Input;
 using Meditrans.Client.Commands;
 using Meditrans.Client.Helpers;
+using Meditrans.Client.Models;
 using Meditrans.Client.Services;
 using Meditrans.Client.Views;
 
@@ -14,17 +16,123 @@ namespace Meditrans.Client.ViewModels
         public ICommand ChangeLanguageCommand { get; }
         public ICommand LogoutCommand { get; }
 
-        public string MenuLogout => LocalizationService.Instance["Logout"];
-        public string MenuChangeLanguage => LocalizationService.Instance["ChangeLanguage"];
+        #region Translation
+
+        // Main menu
+        public string MainMenuHome => LocalizationService.Instance["Home"];
+        public string MainMenuData => LocalizationService.Instance["Data"];
+        public string MainMenuSchedules => LocalizationService.Instance["Schedules"];
+        public string MainMenuDispatch => LocalizationService.Instance["Dispatch"];
+        public string MainMenuReports => LocalizationService.Instance["Reports"];
+        public string MainMenuAdmin => LocalizationService.Instance["Admin"];
+
+
+        public string Settings => LocalizationService.Instance["Settings"];
+        public string MenuChangeLanguage => LocalizationService.Instance["MenuChangeLanguage"];
+        public string MenuEnglishLanguage => LocalizationService.Instance["MenuEnglishLanguage"];
+        public string MenuSpanishLanguage => LocalizationService.Instance["MenuSpanishLanguage"];
+        public string MenuLogout => LocalizationService.Instance["MenuLogout"];
+
+
+        private ObservableCollection<LanguageOption> _languages;
+        public ObservableCollection<LanguageOption> Languages
+        {
+            get => _languages;
+            set
+            {
+                _languages = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private LanguageOption _selectedLanguage;
+        public LanguageOption SelectedLanguage
+        {
+            get => _selectedLanguage;
+            set
+            {
+                _selectedLanguage = value;
+                OnPropertyChanged();
+                ChangeLanguage(value.LanguageCode);
+            }
+        }
+
+        private Visibility _englishCheckVisibility = Visibility.Visible;
+        public Visibility EnglishCheckVisibility
+        {
+            get => _englishCheckVisibility;
+            set
+            {
+                _englishCheckVisibility = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private Visibility _spanishCheckVisibility = Visibility.Collapsed;
+        public Visibility SpanishCheckVisibility
+        {
+            get => _spanishCheckVisibility;
+            set
+            {
+                _spanishCheckVisibility = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+
+
+        #endregion
         public MainWindowViewModel()
         {
             ChangeLanguageCommand = new RelayCommand<string>(ChangeLanguage);
             LogoutCommand = new RelayCommand(Logout);
+
+            Languages = new ObservableCollection<LanguageOption>
+            {
+                new LanguageOption { LanguageCode = "en", DisplayName = MenuEnglishLanguage, FlagEmoji = "🇺🇸" },
+                new LanguageOption { LanguageCode = "es", DisplayName = MenuSpanishLanguage, FlagEmoji = "🇲🇽" },
+                new LanguageOption { LanguageCode = "fr", DisplayName = "Français", FlagEmoji = "🇫🇷" },
+            };
+
+            // Select saved language
+            var currentLang = Properties.Settings.Default.Language ?? "en";
+            var match = Languages.FirstOrDefault(x => x.LanguageCode == currentLang) ?? Languages.First();
+            match.IsSelected = true;
+            SelectedLanguage = match;
+
+            
+            if (SelectedLanguage.LanguageCode.Equals("en")) 
+            {
+                EnglishCheckVisibility = SelectedLanguage.CheckVisibility;
+                SpanishCheckVisibility = Visibility.Collapsed;
+            }else if (SelectedLanguage.LanguageCode.Equals("es"))
+            {
+                SpanishCheckVisibility = SelectedLanguage.CheckVisibility;
+                EnglishCheckVisibility = System.Windows.Visibility.Collapsed;
+            }
         }
 
         private void ChangeLanguage(string languageCode)
         {
-            LocalizationService.Instance.LoadLanguage(languageCode);
+            if (LocalizationService.Instance.CurrentLanguage != languageCode)
+            {
+                LocalizationService.Instance.LoadLanguage(languageCode);
+
+                foreach (var lang in Languages)
+                {
+                    lang.IsSelected = lang.LanguageCode == languageCode;
+                    if (lang.LanguageCode.Equals("en"))
+                    {
+                        EnglishCheckVisibility = lang.CheckVisibility;
+                    }else if (lang.LanguageCode.Equals("es"))
+                    {
+                        SpanishCheckVisibility = lang.CheckVisibility;
+                    }
+                }
+                    
+            }
+            //LocalizationService.Instance.LoadLanguage(languageCode);
         }
 
         private void Logout()
@@ -44,5 +152,6 @@ namespace Meditrans.Client.ViewModels
                 }
             });
         }
+
     }
 }
