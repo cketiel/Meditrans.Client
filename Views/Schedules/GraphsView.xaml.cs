1506bfdb60f2750c6ec14b719dde37f2a9d35053
cketiel <cketielbr@gmail.com>
2025-06-21 02:11:58 -0600

    Add GraphsView to Schedules Tab Control

---
+++ Views/Schedules/GraphsView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Meditrans.Client.Views.Schedules
+{
+    /// <summary>
+    /// Lógica de interacción para GraphsView.xaml
+    /// </summary>
+    public partial class GraphsView : UserControl
+    {
+        public GraphsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
