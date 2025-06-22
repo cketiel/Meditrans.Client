9b724ade8f3bb62bb3b341c70897b92bd614f1b2
cketiel <cketielbr@gmail.com>
2025-06-22 03:53:07 -0600

    Add ColumnConfig Model

---
+++ Models/ColumnConfig.cs
@@ -0,0 +1,19 @@
+﻿
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Meditrans.Client.Models
+{
+    public partial class ColumnConfig : ObservableObject
+    {
+        // Technical name of the property in the DTO, ex: "AuthNo"
+        public string PropertyName { get; set; }
+
+        // Title that the user sees in the grid, ex: "AuthNo" or "Authorization #"
+        [ObservableProperty]
+        private string _header;
+
+        // Whether the column is visible or not
+        [ObservableProperty]
+        private bool _isVisible;
+    }
+}
