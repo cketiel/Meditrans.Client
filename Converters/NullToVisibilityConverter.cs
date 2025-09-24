6fdbacba812d65c97f361c89f3727578782aa44f
cketiel <cketielbr@gmail.com>
2025-09-24 08:27:10 -0600

    Add NullToVisibilityConverter

---
+++ Converters/NullToVisibilityConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Meditrans.Client.Converters
+{
+    public class NullToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // If the value is NULL, it returns Collapsed.
+            //If the value is NOT NULL, returns Visible.
+            return value == null ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
