9c26475ae47af6dd8cea6e645c0f4bdebc2077ff
cketiel <cketielbr@gmail.com>
2025-12-02 14:44:53 -0600

    Add ProductionReportRowDto class

---
+++ DTOs/ProductionReportRowDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Meditrans.Client.DTOs
+{
+    public class ProductionReportRowDto
+    {
+        public DateTime Date { get; set; }
+        public string? Authorization { get; set; }
+        public TimeSpan? ReqPickup { get; set; }
+        public TimeSpan? Appointment { get; set; }
+        public string? Patient { get; set; }
+        public string? PickupCity { get; set; }
+        public string? Run { get; set; }
+        public string? Space { get; set; }
+        public TimeSpan? PickupArrive { get; set; }
+        public TimeSpan? DropoffArrive { get; set; }
+    }
+}
