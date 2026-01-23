c2e7474f187b33acd79bf5403119281a57893c55
cketiel <cketielbr@gmail.com>
2026-01-23 00:11:15 -0600

    Add DTO to History

---
+++ DTOs/TripHistoryCreateDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Meditrans.Client.DTOs
+{
+    public class TripHistoryCreateDto
+    {
+        public int TripId { get; set; }
+        public DateTime ChangeDate { get; set; }
+        public string User { get; set; }
+        public string Field { get; set; }
+        public string? PriorValue { get; set; }
+        public string? NewValue { get; set; }
+    }
+}
